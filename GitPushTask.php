2a6f4719768ff6a35adfff6edfdf85dd1cb29519
Kristofer Widholm <kwidholm@marthastewart.com>
2010-05-17 19:58:08 -0400

    added repository path setter for GitPushTask

--- GitPushTask.php
+++ GitPushTask.php
@@ -9,15 +9,26 @@ require_once 'GitTask.php';
 class GitPushTask extends GitTask {
 	private $_remote;
 	private $_branch;
+	private $_path;
 
 	/**
 	 * Main entry point.
 	 */
 	public function main() {
+		$current = getcwd();
+		chdir($this->_path);
 		$command = $this->git_path . " push " . $this->_remote . " " . $this->_branch;
 		$this->log("Pushing: " . $command);
 		passthru($command, $return);
+		chdir($current);
 		$this->log("Push Return: " . $return);
+	}
+
+	/**
+	 * Sets the target path for the repository to push
+	 */
+	public function setPath($path) {
+		$this->_path = $path;
 	}
 
 	/**
